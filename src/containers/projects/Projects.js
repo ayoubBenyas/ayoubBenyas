d663405ccecd675a4b725cfa5417955bfe9d845c
ioubnyacs <aubbenyas717@gmail.com>
2020-05-11 15:01:32 +0000

    api.github - card

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-import ApolloClient from "apollo-boost";
-import { gql } from "apollo-boost";
 import "./Project.css";
 import GithubRepoCard from "../../components/githubRepoCard/GithubRepoCard";
 import Button from "../../components/button/Button";
@@ -8,63 +6,27 @@ import { openSource } from "../../portfolio";
 import { Fade } from "react-reveal";
 
 export default function Projects() {
-  const [repo, setrepo] = useState([]);
+  const [repos, setrepos] = useState([]);
 
   useEffect(() => {
     getRepoData();
   }, []);
 
   function getRepoData() {
-    const client = new ApolloClient({
-      uri: "https://api.github.com/graphql",
-      request: operation => {
-        operation.setContext({
-          headers: {
-            authorization: `Bearer ${atob(openSource.githubConvertedToken)}`
-          }
-        });
-      }
-    });
+    
 
-    client
-      .query({
-        query: gql`
-        {
-        user(login: "${openSource.githubUserName}") {
-          pinnedItems(first: 6, types: [REPOSITORY]) {
-            totalCount
-            edges {
-              node {
-                ... on Repository {
-                  name
-                  description
-                  forkCount
-                  stargazers {
-                    totalCount
-                  }
-                  url
-                  id
-                  diskUsage
-                  primaryLanguage {
-                    name
-                    color
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-        `
-      })
-      .then(result => {
-        setrepoFunction(result.data.user.pinnedItems.edges);
-        console.log(result);
-      });
+    fetch(`https://api.github.com/users/${openSource.githubUserName}/repos`)
+      .then(res => res.json())
+        .then(
+          (result) => {
+            setreposFunction(result);
+            console.log(result);
+            //alert(JSON.stringify(result))
+          });  
   }
 
-  function setrepoFunction(array) {
-    setrepo(array);
+  function setreposFunction(array) {
+    setrepos(array);
   }
 
   return (
@@ -72,8 +34,10 @@ export default function Projects() {
     <div className="main" id="opensource">
       <h1 className="project-title">Open Source Projects</h1>
       <div className="repo-cards-div-main">
-        {repo.map((v, i) => {
-          return <GithubRepoCard repo={v} key={v.node.id} />;
+        {repos.map((r, i) => {
+          if( !r.fork ){
+            return <GithubRepoCard repo={r} />;
+          }
         })}
       </div>
       <Button text={"More Projects"} className="project-button" href="https://github.com/ayoubBenyas" newTab={true} />
--- src/index.js
+++ src/index.js
@@ -12,3 +12,16 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBajQ1VvHF4jyv7u6U0Kmt9H8DXb3Ar-2Y ",
+    authDomain: "project-934168468131.firebaseapp.com",
+    databaseURL: "https://project-934168468131.firebaseio.com",
+    projectId: "project-934168468131",
+    storageBucket: "project-934168468131.appspot.com",
+    messagingSenderId: "sender-id",
+    appId: "1:934168468131:web:ed4aa86b162bd37aa4fd16",
+    measurementId: "G-measurement-id",
+  };
+  
